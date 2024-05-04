39276a604c7f0b1b7f5f002ba38ce6bc464f6589
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-03 19:15:53 -0500

    new task

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,7 +8,6 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 from models.state import State
-from werkzeug.exceptions import BadRequest, UnsupportedMediaType
 
 
 @app_views.route('/states', methods=['GET'])
@@ -49,3 +48,22 @@ def process_json():
     storage.new(new_instance)
     storage.save()
     return jsonify(new_instance.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_obj(state_id):
+    instance = storage.get(State, state_id)
+    json_data = request.get_json()
+
+    if instance is None:
+        return jsonify({"error": "Not found"}), 404
+
+    if json_data is None:
+        return jsonify({"error": "Not a JSON"}), 400
+
+    
+    for key, value in json_data.items():
+        setattr(instance, key, value)
+    storage.save()
+
+    return jsonify(instance.to_dict()), 200
